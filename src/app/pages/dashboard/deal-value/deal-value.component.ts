6e0062496c650f86577bfa466bdfb8cc89a0a004
Puneeth <tp004@himalayawellness.com>
2025-04-08 10:31:59 +0530

    architecture

---
+++ src/app/pages/dashboard/deal-value/deal-value.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deal-value',
+  imports: [],
+  templateUrl: './deal-value.component.html',
+  styleUrl: './deal-value.component.scss'
+})
+export class DealValueComponent {
+
+}
---
+++ src/app/pages/dashboard/high-value-deals/high-value-deals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-high-value-deals',
+  imports: [],
+  templateUrl: './high-value-deals.component.html',
+  styleUrl: './high-value-deals.component.scss'
+})
+export class HighValueDealsComponent {
+
+}
